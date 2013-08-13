2bbfbb667dea2661c170693c58249fea86fef46a
wes hayutin <whayutin@redhat.com>
2013-08-13 10:43:25 -0400

    add packstack script testing

--- packstack.bash
+++ packstack.bash
@@ -1,12 +1,13 @@
 export GROUP_TYPE=libvirt
-export DISTRO_NAME=rhel
+export DISTRO_NAME=centos
 rake kytoon:create GROUP_CONFIG="config/server_group.json"
 
 
 # required for Torpedo
 #rake build_misc
 #rake torpedo:build_packages
-rake build_packstack --trace
+#rake build_packstack --trace
+rake rhel:build_packstack --trace
 
-rake create_package_repo
+#rake create_package_repo
 
